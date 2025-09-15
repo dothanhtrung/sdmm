6fa9e32135e187b84293783c774c0042424f0da1
Trung Do <dothanhtrung@pm.me>
2025-09-15 22:36:45 +0700

    db: Add functions to get or clean job

--- src/db/job.rs
+++ src/db/job.rs
@@ -1,7 +1,6 @@
-
+use serde::Serialize;
 use sqlx::{Error, SqlitePool};
 use std::time::SystemTime;
-use serde::Serialize;
 
 #[derive(Serialize)]
 #[repr(i64)]
@@ -9,6 +8,15 @@ pub enum JobState {
     Running,
     Succeed,
     Failed,
+}
+
+pub struct Job {
+    pub id: i64,
+    pub title: String,
+    pub desc: String,
+    pub state: i64,
+    pub started_at: i64,
+    pub stopped_at: Option<i64>,
 }
 
 pub async fn add_job(pool: &SqlitePool, title: &str, desc: &str) -> Result<i64, Error> {
@@ -20,7 +28,8 @@ pub async fn add_job(pool: &SqlitePool, title: &str, desc: &str) -> Result<i64, 
         JobState::Running as i64
     )
     .fetch_one(pool)
-    .await?.id;
+    .await?
+    .id;
     Ok(id)
 }
 
@@ -37,4 +46,29 @@ pub async fn update_job(pool: &SqlitePool, id: i64, desc: &str, state: JobState)
     .execute(pool)
     .await?;
     Ok(())
+}
+
+pub async fn get(pool: &SqlitePool, limit: i64, offset: i64) -> Result<(Vec<Job>, i64), sqlx::Error> {
+    let items = sqlx::query_as!(
+        Job,
+        r#"SELECT * FROM job ORDER BY started_at DESC LIMIT ? OFFSET ?"#,
+        limit,
+        offset
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let total = sqlx::query_scalar!("SELECT count(id) FROM job",)
+        .fetch_one(pool)
+        .await?;
+
+    Ok((items, total))
+}
+
+pub async fn clean(pool: &SqlitePool) -> Result<u64, sqlx::Error> {
+    let count = sqlx::query!(r#"DELETE FROM job WHERE stopped_at IS NOT NULL"#)
+        .execute(pool)
+        .await?
+        .rows_affected();
+    Ok(count)
 }

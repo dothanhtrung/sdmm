7950ecb83267f388f2f10efdebc9515cbdfcda48
Trung Do <dothanhtrung@pm.me>
2026-03-15 23:28:10 +0700

    SSE: Reconnect on lost connection

--- src/ui.rs
+++ src/ui.rs
@@ -103,8 +103,6 @@ impl Broadcaster {
     pub async fn new_client(&self) -> Sse<InfallibleStream<ReceiverStream<sse::Event>>> {
         let (tx, rx) = mpsc::channel(10);
 
-        tx.send(sse::Data::new("connected").into()).await.unwrap();
-
         self.inner.lock().clients.push(tx);
 
         Sse::from_infallible_receiver(rx)
